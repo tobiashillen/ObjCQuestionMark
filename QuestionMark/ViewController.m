a27e0a2f0c07998d1db77060231ef40c81c5f031
tobiashillen <tobias.hillen@gmail.com>
2017-01-26 15:23:32 +0100

    Gesture Demo.

--- QuestionMark/ViewController.m
+++ QuestionMark/ViewController.m
@@ -10,13 +10,48 @@
 
 @interface ViewController ()
 
+@property (nonatomic) UILabel *questionMark;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.questionMark = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 50, 50)];
+    self.questionMark.center = self.view.center;
+    self.questionMark.text = @"?";
+    self.questionMark.font = [UIFont systemFontOfSize:36];
+    self.questionMark.textAlignment = NSTextAlignmentCenter;
+    [self.view addSubview:self.questionMark];
+    
+    UITapGestureRecognizer *tapGestureRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tap:)];
+    
+    [self.view addGestureRecognizer:tapGestureRecognizer];
+
+}
+
+- (void) tap:(UITapGestureRecognizer*)regognizer {
+    CGPoint tapPosition = [regognizer locationInView:self.view];
+    self.questionMark.center = tapPosition;
+}
+- (float)randomFloat {
+    return (arc4random() / (float)UINT_MAX);
+}
+
+- (IBAction)swipe:(UISwipeGestureRecognizer *)sender {
+    self.questionMark.textColor = [UIColor colorWithRed:[self randomFloat] green:[self randomFloat] blue:[self randomFloat] alpha:1.0];
+}
+- (IBAction)pinch:(UIPinchGestureRecognizer *)sender {
+    CGAffineTransform newScale = CGAffineTransformScale(self.questionMark.transform, sender.scale, sender.scale);
+    self.questionMark.transform = newScale;
+    sender.scale = 1;
+}
+
+- (IBAction)rotation:(UIRotationGestureRecognizer *)sender {
+    CGAffineTransform newRotation = CGAffineTransformRotate(self.questionMark.transform, sender.rotation);
+    self.questionMark.transform = newRotation;
+    sender.rotation = 0.0;
 }
 
 
